b4134c974668fafd75976338d962f2c27b50c46e
ValeryKot <kot.valery@gmail.com>
2022-12-16 10:56:31 +0300

    add enzyme

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
-import React from 'react';
-import Pic from './Pic';
+import React, { Component } from 'react';
+// import logo from './logo.svg';
+import './App.css';
+import SimpleComponent from './SimpleComponent';
 
-const App = () => {
-  return (
-    <div>
-      <Pic size={300} />
-      <Pic round />
-    </div>
-  );
-};
+
+class App extends Component {
+  componentDidMount() {
+    this.setState({ name: "My app!" });
+  }
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          {/* <img src={logo} className="App-logo" alt="logo" /> */}
+          <h2>Welcome to React</h2>
+        </div>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+        <SimpleComponent first="Matt" last="Lane" favFood="ice cream" />
+      </div>
+    );
+  }
+}
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,36 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import App from '../App';
+import toJson from 'enzyme-to-json'; // for snapshotting the component
+import SimpleComponent from '../SimpleComponent';
+import { shallow, mount } from 'enzyme';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+describe('<App />', () => {
+  let wrapper;
+
+  it('renders without crashing', () => {
+    wrapper = shallow(<App />);
+  });
+
+  it('matches snapshot', () => {
+    expect(toJson(wrapper)).toMatchSnapshot();
+  });
+
+  it('should render one <SimpleComponent /> component', () => {
+    expect(wrapper.find(SimpleComponent)).toHaveLength(1);
+    expect(wrapper.find('h2')).toHaveLength(1);
+  });
+
+  it('should render with a class of App-intro', () => {
+    expect(wrapper.find('.App-intro')).toHaveLength(1);
+  });
+
+  it('should detect no state on shallow rendering', () => {
+    expect(wrapper.state()).toBeNull();
+  });
+
+  it('should detect state and all h2s on mounting', () => {
+    wrapper = mount(<App />);
+    expect(wrapper.state('name')).toBe('My app!');
+    expect(wrapper.find('h2')).toHaveLength(2);
+  });
 });
