0905a1c979e2c4160b65d045961d0fad4d0a53ea
ValeryKot <kot.valery@gmail.com>
2022-12-16 12:05:23 +0300

    finish routes

--- react-router-demo/src/App.js
+++ react-router-demo/src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { Route, Link } from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const Home = props => (
+  <div>
+    <h2>Hello {props.name}! </h2>
+  </div>
+);
+
+const About = () => (
+  <div>
+    <h2>About</h2>
+  </div>
+);
+
+const App = () => (
+  <div>
+    <ul>
+      <li>
+        <Link to="/">Home</Link>
+      </li>
+      <li>
+        <Link to="/about">About</Link>
+      </li>
+    </ul>
+    <Route
+      exact
+      path="/"
+      component={props => <Home name="Elie" {...props} />}
+    />
+    <Route path="/about" component={About} />
+  </div>
+);
 
 export default App;
