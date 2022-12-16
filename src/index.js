e591ae05edfa9c2e09e4dde1da772de070d0f161
ValeryKot <kot.valery@gmail.com>
2022-12-16 10:08:12 +0300

    styled lesson

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <App name="Alice" age={30} />
   </React.StrictMode>
 );
 
